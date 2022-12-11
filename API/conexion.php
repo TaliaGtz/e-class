f8f7048cb0eeaa79fea811b40281146933e787c4
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-12-11 15:33:13 -0600

    inicio de sesión

---
+++ API/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$link = 'mysql:host=localhost;dbname=e-class';
+$usuario = 'root';
+$pass = '';
+
+try{
+    $pdo = new PDO($link, $usuario, $pass);
+    //echo 'conectado';
+}catch(PDOException $e){
+    print "Error: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
